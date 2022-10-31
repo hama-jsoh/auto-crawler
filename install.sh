e580471b345424dc4ea9bed3bc093def98e7c55e
hama-jsoh <111451710+hama-jsoh@users.noreply.github.com>
2022-11-01 00:01:17 +0900

    :zap: Add argument yes

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # install google chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome-stable_current_amd64.deb
+sudo apt install -y ./google-chrome-stable_current_amd64.deb
 
 CHROME_VERSION=`google-chrome --version | awk '{print $3}'`
 CHROME_DRIVER_VERSION=`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`
