7865de862dcdadf45423ec15a573f68861477be7
hama-jsoh <111451710+hama-jsoh@users.noreply.github.com>
2022-11-01 00:07:16 +0900

    :zap: Add rm options

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@
 
 # install google chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install -y ./google-chrome-stable_current_amd64.deb
+sudo apt install -y ./google-chrome-stable_current_amd64.deb \
+ && rm -rf ./google-chrome-stable_current_amd64.deb
 
 CHROME_VERSION=`google-chrome --version | awk '{print $3}'`
 CHROME_DRIVER_VERSION=`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`
@@ -10,16 +11,17 @@ CHROME_DRIVER_VERSION=`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEA
 VERSION=${CHROME_VERSION:0:3}
 DRIVER_VERSION=${CHROME_DRIVER_VERSION:0:3}
 
+# install chromedriver
 install_chromedriver() {
 
-    # install chromedriver
     if [[ $VERSION -eq $DRIVER_VERSION ]]; then
     
         execute \
             "wget -N \
             http://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip \
             -P ." \
-            "unzip chromedriver_linux64.zip -d ./chromedriver/"
+            "unzip chromedriver_linux64.zip -d ./chromedriver/" \
+            "rm -rf chromedriver_linux64.zip"
             
     else
         echo "chromedriver_version does not match the google-chrome_version"
