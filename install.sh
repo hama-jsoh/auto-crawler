29cc4763a0f3eb8a5266ee3e4f9833f05236a15b
hama-jsoh <111451710+hama-jsoh@users.noreply.github.com>
2022-11-01 15:39:46 +0900

    :memo: Chore

--- install.sh
+++ install.sh
@@ -19,8 +19,8 @@ install_chromedriver() {
             "wget -N http://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip -P . \
             && unzip chromedriver_linux64.zip -d ./chromedriver/ \
             && rm chromedriver_linux64.zip" \
-	       && print_success "google-chrome version: $CHROME_VERSION" \
-	       && print_success "chromedriver version: $CHROME_DRIVER_VERSION"
+	    && print_success "google-chrome version: $CHROME_VERSION" \
+	    && print_success "chromedriver version: $CHROME_DRIVER_VERSION"
     else
         echo "chromedriver_version does not match the google-chrome_version"
     fi
